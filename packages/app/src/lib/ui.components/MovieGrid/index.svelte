cca85eecd6b42d88ffe2994a99dd6ee9c129096f
andrew robertson <andrewcrobertson@gmail.com>
2024-10-23 22:48:27 +1100

    wip

--- packages/app/src/lib/ui.components/MovieGrid/index.svelte
+++ packages/app/src/lib/ui.components/MovieGrid/index.svelte
@@ -6,14 +6,14 @@
 
 <h1 class="text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold pl-1 pt-3 pb-1">{title}</h1>
   <!-- Note: scrollbar-hide is a Tailwind custom utility -->
-  <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 pr-2 overflow-y-auto scrollbar-hide">
+  <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-2 pr-2 overflow-y-auto scrollbar-hide">
   {#each movies as { id, title, posterFileName, hasSubtitles }}
     <a href={`${base}/view/${id}`} class="group block relative">
       <img src={`${base}/posters/${posterFileName}`} alt={title} class="w-full h-full object-cover" />
 
       {#if !hasSubtitles}
         <div class="absolute bottom-0 left-0 right-0 bg-red-500 bg-opacity-70 text-white text-center py-1 text-sm">
-          No subtitles available
+          No subtitles
         </div>
       {/if}
     </a>
