764de3a963335017e798775f23f09961002b8e83
andrew robertson <andrewcrobertson@gmail.com>
2024-10-26 10:13:56 +1100

    wip

--- packages/app/src/routes/my-list/+page.svelte
+++ packages/app/src/routes/my-list/+page.svelte
@@ -2,11 +2,10 @@
   import { base } from '$app/paths';  
   import Header from '$lib/ui.components/Header';
   import { MyListManager } from '$lib/ui.services/MyListManager';
-  import { formatIsoDate } from '$lib/ui.utils/format';
+  import { formatRunTime, formatTextArray, formatIsoDate, formatText } from '$lib/ui.utils/format';
     import { onMount } from 'svelte';
   import type { PageData } from './$types';
   import { includes } from 'lodash-es';
-  import ChevronRightIcon from '$lib/ui.icons/ChevronRightIcon.svelte';
   import { fade } from 'svelte/transition';
   
   export let data: PageData;
@@ -36,7 +35,7 @@
   {#if loaded}
     <div transition:fade={{ duration: 500 }}>
       {#if myListMovies.length > 0}
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pr-2 overflow-y-auto scrollbar-hide">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pr-2 overflow-y-auto">
           {#each myListMovies as { id, title, releaseDate, posterFileName, rated, genres, actors, runTime, plot }}
             <div class="flex items-start overflow-hidden">
               <a href={`${base}/subtitles/${id}`} class="flex-shrink-0">
@@ -45,11 +44,11 @@
               <div class="pl-4 flex-1">
                 <h3 class="text-lg font-semibold text-white">{title}</h3>
                 <!-- <p class="text-sm  text-white">{plot}</p> -->
-                <p class="text-sm text-gray-400">Release Date: {formatIsoDate(releaseDate)}</p>
-                <p class="text-sm text-gray-400">Rated: {rated}</p>
-                <p class="text-sm text-gray-400">Genres: {genres.join(', ')}</p>
-                <p class="text-sm text-gray-400">Actors: {actors.join(', ')}</p>
-                <p class="text-sm text-gray-400">Runtime: {runTime} min</p>
+                <p class="text-sm text-gray-400">Released: {formatIsoDate(releaseDate, 'Unknown')}</p>
+                <p class="text-sm text-gray-400">Rated: {formatText(rated, 'Unknown')}</p>
+                <p class="text-sm text-gray-400">Genres: {formatTextArray(genres, 'Unknown')}</p>
+                <p class="text-sm text-gray-400">Actors: {formatTextArray(actors, 'Unknown')}</p>
+                <p class="text-sm text-gray-400">Runtime: {formatRunTime(runTime, 'Unknown')}</p>
               </div>
             </div>
           {/each}
@@ -58,10 +57,3 @@
     </div>
   {/if}
 </div>
-
-<style>
-  /* Add your custom styles here */
-  .scrollbar-hide::-webkit-scrollbar {
-    display: none;
-  }
-</style>
