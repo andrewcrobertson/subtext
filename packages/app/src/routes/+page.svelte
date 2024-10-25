0732628fbed2afb9201dc22102ec7571c029ad07
andrew robertson <andrewcrobertson@gmail.com>
2024-10-26 08:44:23 +1100

    wip

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+  import { base } from '$app/paths';  
   import Header from '$lib/ui.components/Header';
-  import MovieGrid from '$lib/ui.components/MovieGrid';
   import { BookmarkManager } from '$lib/ui.services/BookmarkManager';
   import type{ BookmarkedMovieEventDetail } from '$lib/ui.types/BookmarkedMovieEventDetail';
   import { onMount } from 'svelte';
@@ -35,9 +35,19 @@
   <Header class="fixed top-0 left-0 right-0" />
   <div class="mt-16"></div>
   {#if movies.length > 0}
-    <MovieGrid {movies} on:pinclick={(e) => handleUnbookmarkClick(e)} />
+    <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-2 pr-2 overflow-y-auto scrollbar-hide">
+      {#each movies as { id, title, posterFileName, hasSubtitles }}
+        <a href={`${base}/subtitles/${id}`} class="group block relative">
+          <img src={`${base}/posters/${posterFileName}`} alt={title} class="w-full h-full object-cover" />
+          {#if !hasSubtitles}
+            <div class="absolute bottom-0 left-0 right-0 bg-red-500 bg-opacity-70 text-white text-center py-1 text-sm">
+              No subtitles
+            </div>
+          {/if}
+        </a>
+      {/each}
+    </div>
   {:else}
     <p class="text-white">You don't have any book marked movies.</p>
   {/if}
-
 </div>
--- packages/app/src/routes/search/+page.svelte
+++ packages/app/src/routes/search/+page.svelte
@@ -1,43 +1,56 @@
 <script lang="ts">
+  import { base } from '$app/paths';  
   import Header from '$lib/ui.components/Header';
-  import MovieGrid from '$lib/ui.components/MovieGrid';
-  import { BookmarkManager } from '$lib/ui.services/BookmarkManager';
-  import type{ BookmarkedMovieEventDetail } from '$lib/ui.types/BookmarkedMovieEventDetail';
-  import { onMount } from 'svelte';
   import type { PageData } from './$types';
-    import { includes } from 'lodash-es';
   export let data: PageData;
 
-  let BookmarkedMovies: any[] = [];
-  const bookmarkManager = new BookmarkManager()
+  let expandedId: string | null = null;
 
-  const handleBookmarkClick = ({ detail }: CustomEvent<BookmarkedMovieEventDetail>) => {
-    bookmarkManager.bookmarkMovie(detail.id)
-    loadBookmarkedMovies();
+  function toggleExpand(id: string) {
+    expandedId = expandedId === id ? null : id;
   }
 
-  const handleUnbookmarkClick = ({ detail }: CustomEvent<BookmarkedMovieEventDetail>) => {
-    bookmarkManager.unbookmarkMovie(detail.id)
-    loadBookmarkedMovies();
+  function handleTouchStart(event: TouchEvent, id: string) {
+    event.preventDefault();
+    toggleExpand(id);
   }
-
-  const loadBookmarkedMovies = () => {
-    let tempBookmarkedMovies: any[] = [];
-    const BookmarkedMovieIds = bookmarkManager.getAllBookmarkedMovies()
-
-    for(let i = 0; i < data.movies.length; i++) {
-      const movie = data.movies[i];
-      if(includes(BookmarkedMovieIds, movie.id)) tempBookmarkedMovies.push(movie)
-    }
-
-    BookmarkedMovies = tempBookmarkedMovies
-  }
-
-  onMount(() => loadBookmarkedMovies());
 </script>
 
 <div class="relative">
   <Header class="fixed top-0 left-0 right-0" />
   <div class="mt-16"></div>
-  <MovieGrid movies={data.movies} on:pinclick={(e) => handleUnbookmarkClick(e)} />
+  {#if data.movies.length > 0}
+    <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-2 pr-2 overflow-y-auto scrollbar-hide -my-10 py-10">
+      {#each data.movies as { id, title, posterFileName, hasSubtitles }}
+        <!-- svelte-ignore a11y_click_events_have_key_events -->
+        <!-- svelte-ignore a11y_no_static_element_interactions -->
+        <div 
+          on:touchstart={(e) => handleTouchStart(e, id)}
+          on:click|stopPropagation={() => toggleExpand(id)}
+          class={`relative group cursor-pointer transition-transform duration-300 ease-in-out shadow-lg ${
+            expandedId === id ? 'scale-[1.15] z-30' : 'z-0'
+          }`}
+        >
+          <img src={`${base}/posters/${posterFileName}`} alt={title} class="w-full h-full object-cover transition-transform duration-300 ease-in-out" />
+          {#if !hasSubtitles}
+            <div class="absolute bottom-0 left-0 right-0 bg-red-500 bg-opacity-70 text-white text-center py-1 text-sm">
+              No subtitles
+            </div>
+          {/if}
+          {#if expandedId === id}
+            <div on:click|stopPropagation={() => alert(`Bookmarked movie: ${title}`)} class="absolute top-0 w-full bg-black bg-opacity-60 text-white py-2 flex justify-center items-center transition-opacity duration-300">
+              <button 
+                class="text-lg font-semibold text-white bg-transparent  cursor-pointer z-40"
+                on:click|stopPropagation={() => alert(`Bookmarked movie: ${title}`)}
+              >
+                Bookmark
+              </button>
+            </div>
+          {/if}
+        </div>
+      {/each}
+    </div>
+  {:else}
+    <p class="text-white">Could not find any movies.</p>
+  {/if}
 </div>
