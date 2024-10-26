5147dfb13a0466d7b79ab58129350893911d100b
andrew robertson <andrewcrobertson@gmail.com>
2024-10-26 13:35:33 +1100

    wip

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { base } from '$app/paths';
   import Header from '$lib/ui.components/Header';
+  import TransitionLoad from '$lib/ui.components/TransitionLoad';
   import { MyListManager } from '$lib/ui.services/MyListManager';
   import { onMount } from 'svelte';
   import type { PageData } from './$types';
@@ -35,39 +36,35 @@
   });
 </script>
 
-<div class="relative">
-  <Header class="fixed top-0 left-0 right-0" />
-  <div class="mt-16"></div>
-  {#if loaded}
-    <div transition:fade={{ duration: 500 }}>
-      {#if myListMovies.length > 0}
-        <div class="flex justify-between items-center py-4 px-2">
-          <h2 class="text-white text-xl md:text-2xl lg:text-3xl font-semibold">My List</h2>
-          <a href={`${base}/my-list`} class="flex items-center text-white text-xl md:text-2xl lg:text-3xl font-semibold">
-            Edit
-            <ChevronRightIcon class="font-semibold" />
-          </a>
-        </div>
-        <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-2 pr-2 overflow-y-auto scrollbar-hide">
-          {#each myListMovies as { id, title, posterFileName }}
-            <PosterLink href={`${base}/subtitles/${id}`} src={`${base}/posters/${posterFileName}`} alt={title} />
-          {/each}
-        </div>
-      {/if}
-      {#if recentMovies.length > 0}
-        <div class="flex justify-between items-center py-4 px-2">
-          <h2 class="text-white text-xl md:text-2xl lg:text-3xl font-semibold">Recent</h2>
-          <a href={`${base}/search`} class="flex items-center text-white text-xl md:text-2xl lg:text-3xl font-semibold">
-            View All
-            <ChevronRightIcon class="font-semibold" />
-          </a>
-        </div>
-        <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-2 pr-2 overflow-y-auto scrollbar-hide">
-          {#each recentMovies as { id, title, posterFileName }}
-            <PosterLink href={`${base}/subtitles/${id}`} src={`${base}/posters/${posterFileName}`} alt={title} />
-          {/each}
-        </div>
-      {/if}
+<Header class="fixed top-0 left-0 right-0" />
+<div class="mt-16"></div>
+<TransitionLoad {loaded}>
+  {#if myListMovies.length > 0}
+    <div class="flex justify-between items-center py-4 px-2">
+      <h2 class="text-white text-xl md:text-2xl lg:text-3xl font-semibold">My List</h2>
+      <a href={`${base}/my-list`} class="flex items-center text-white text-xl md:text-2xl lg:text-3xl font-semibold">
+        Edit
+        <ChevronRightIcon class="font-semibold" />
+      </a>
+    </div>
+    <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-2 pr-2 overflow-y-auto scrollbar-hide">
+      {#each myListMovies as { id, title, posterFileName }}
+        <PosterLink href={`${base}/subtitles/${id}`} src={`${base}/posters/${posterFileName}`} alt={title} />
+      {/each}
     </div>
   {/if}
-</div>
+  {#if recentMovies.length > 0}
+    <div class="flex justify-between items-center py-4 px-2">
+      <h2 class="text-white text-xl md:text-2xl lg:text-3xl font-semibold">Recent</h2>
+      <a href={`${base}/search`} class="flex items-center text-white text-xl md:text-2xl lg:text-3xl font-semibold">
+        View All
+        <ChevronRightIcon class="font-semibold" />
+      </a>
+    </div>
+    <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-2 pr-2 overflow-y-auto scrollbar-hide">
+      {#each recentMovies as { id, title, posterFileName }}
+        <PosterLink href={`${base}/subtitles/${id}`} src={`${base}/posters/${posterFileName}`} alt={title} />
+      {/each}
+    </div>
+  {/if}
+</TransitionLoad>
