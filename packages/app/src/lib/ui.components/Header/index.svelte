be3b9b583b01dc4d4b76439bb8a1fdb52beb9e49
andrew robertson <andrewcrobertson@gmail.com>
2024-10-26 15:26:34 +1100

    wip

--- packages/app/src/lib/ui.components/Header/index.svelte
+++ packages/app/src/lib/ui.components/Header/index.svelte
@@ -3,20 +3,8 @@
   import { base } from '$app/paths';
   import ChatBubbleLeftRightIcon from '$lib/ui.icons/ChatBubbleLeftRightIcon.svelte';
   import QuestionMarkCircleIcon from '$lib/ui.icons/QuestionMarkCircleIcon.svelte';
-  import { onMount } from 'svelte';
   import { twMerge } from 'tailwind-merge';
   export { _class as class };
-
-  let canInstall = false;
-
-  const handleInstallClick = () => window.beforeInstallPromptEvent.prompt();
-
-  onMount(() => {
-    const isFullscreen = window.matchMedia('(display-mode: fullscreen)').matches;
-    const isMinimalUi = window.matchMedia('(display-mode: minimal-ui)').matches;
-    const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
-    if (!isFullscreen && !isMinimalUi && !isStandalone) canInstall = true;
-  });
 </script>
 
 <div class={twMerge('flex justify-between items-center p-2 z-10 bg-black bg-opacity-70 border-b-2 border-yellow-500', _class)}>
