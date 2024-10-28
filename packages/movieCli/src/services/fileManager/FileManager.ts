de2254d74de966fd4883b883248c5bbb5853b2df
andrew robertson <andrewcrobertson@gmail.com>
2024-10-28 11:25:10 +1100

    wip

--- packages/movieCli/src/services/fileManager/FileManager.ts
+++ packages/movieCli/src/services/fileManager/FileManager.ts
@@ -9,31 +9,27 @@ export class FileManager {
   public constructor(private readonly dir: string) {}
 
   public async getMovieData(imdbId: string) {
-    const rootDir = path.resolve(this.dir, imdbId);
-    const file = path.resolve(rootDir, 'index.json');
-    const data = fs.existsSync(file) ? <T.GetIndexResponse>JSON.parse(fs.readFileSync(file, 'utf-8')) : null;
+    const filePath = this.getMovieDataFilePath(imdbId);
+    const data = fs.existsSync(filePath) ? <T.GetIndexResponse>JSON.parse(fs.readFileSync(filePath, 'utf-8')) : null;
     return data;
   }
 
   public async writeMovieData(data: T.WriteMovieDataInputMovie, timestamp: string) {
-    const rootDir = path.resolve(this.dir, data.imdbId);
-    const file = path.resolve(rootDir, 'index.json');
-    await this.writeJsonFile(file, data, timestamp);
-    return file;
+    const filePath = this.getMovieDataFilePath(data.imdbId);
+    await this.writeJsonFile(filePath, data, timestamp);
+    return filePath;
   }
 
   public async writeSubtitleData(imdbId: string, data: T.WriteSubtitleDataInputSubtitle, timestamp: string) {
-    const rootDir = path.resolve(this.dir, imdbId, 'subtitles', data.subtitleId);
-    const file = path.resolve(rootDir, 'index.json');
-    await this.writeJsonFile(file, data, timestamp);
-    return file;
+    const filePath = this.getSubtitleDataFilePath(imdbId, data.subtitleId);
+    await this.writeJsonFile(filePath, data, timestamp);
+    return filePath;
   }
 
-  public async writeSubtitleText(imdbId: string, data: T.WriteSubtitleDataInputSubtitle, text: string, timestamp: string) {
-    const rootDir = path.resolve(this.dir, imdbId, 'subtitles', data.subtitleId);
-    const file = path.resolve(rootDir, data.subTextFileName);
-    await this.writeTextFile(file, text);
-    return file;
+  public async writeSubtitleText(imdbId: string, data: T.WriteSubtitleDataInputSubtitle, text: string, _timestamp: string) {
+    const filePath = this.getSubtitleTextFilePath(imdbId, data.subtitleId, data.subTextFileName);
+    await this.writeTextFile(filePath, text);
+    return filePath;
   }
 
   public async writePoster(imdbId: string, posterFileName: string, posterUrl: string) {
@@ -43,19 +39,48 @@ export class FileManager {
     return posterFile;
   }
 
-  public async writeImageFile(filePath: string, url: string) {
+  private getMovieDir(imdbId: string) {
+    const rootDir = path.resolve(this.dir, imdbId);
+    return rootDir;
+  }
+
+  private getSubtitlesDir(imdbId: string, subtitleId: string) {
+    const movieDir = this.getMovieDir(imdbId);
+    const subtitlesDir = path.resolve(movieDir, 'subtitles', subtitleId);
+    return subtitlesDir;
+  }
+
+  private getMovieDataFilePath(imdbId: string) {
+    const rootDir = this.getMovieDir(imdbId);
+    const filePath = path.resolve(rootDir, 'index.json');
+    return filePath;
+  }
+
+  private getSubtitleDataFilePath(imdbId: string, subtitleId: string) {
+    const subtitlesDir = this.getSubtitlesDir(imdbId, subtitleId);
+    const filePath = path.resolve(subtitlesDir, 'index.json');
+    return filePath;
+  }
+
+  private getSubtitleTextFilePath(imdbId: string, subtitleId: string, fileName: string) {
+    const subtitlesDir = this.getSubtitlesDir(imdbId, subtitleId);
+    const filePath = path.resolve(subtitlesDir, fileName);
+    return filePath;
+  }
+
+  private async writeImageFile(filePath: string, url: string) {
     this.ensureDir(filePath);
     const response = await fetch(url);
     const fileStream = fs.createWriteStream(filePath);
     await promisify(pipeline)(response.body as unknown as NodeJS.ReadableStream, fileStream);
   }
 
-  public async writeTextFile(filePath: string, fileContent: string) {
+  private async writeTextFile(filePath: string, fileContent: string) {
     this.ensureDir(filePath);
     fs.writeFileSync(filePath, fileContent);
   }
 
-  public async writeJsonFile(filePath: string, fileContent: any, timestamp: string) {
+  private async writeJsonFile(filePath: string, fileContent: any, timestamp: string) {
     this.ensureDir(filePath);
 
     const newFileContent = cloneDeep(fileContent);
