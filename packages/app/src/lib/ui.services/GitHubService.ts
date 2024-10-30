0bb2271a0411522dcea28ad4235027c59495b2b3
andrew robertson <andrewcrobertson@gmail.com>
2024-10-31 09:03:30 +1100

    wip

--- packages/app/src/lib/ui.services/GitHubService.ts
+++ packages/app/src/lib/ui.services/GitHubService.ts
@@ -8,13 +8,12 @@ export class GitHubService {
 
   public async submitAddMovieRequestIssue(userId: string, imdbId: string) {
     try {
-      const lines = [`:id: ${userId}`, ':robot: This issue is automated.', ":pray: Please don't edit this issue."];
-      const issueData = { title: imdbId, body: join(lines, '\n'), labels: ['add'] };
+      const lines = [':robot: This issue is automated.', '', '===', '', 'type: REQUEST_MOVIE', `userId: ${userId}`, `imdbIdId: ${imdbId}`];
 
       const url = `${this.baseApi}/issues`;
       const headers = { Authorization: `token ${this.token}`, Accept: 'application/vnd.github+json', 'Content-Type': 'application/json' };
-      const body = JSON.stringify(issueData);
-      const response = await fetch(url, { method: 'POST', headers, body });
+      const body = { title: imdbId, body: join(lines, '\n'), labels: ['subtext-bot'] };
+      const response = await fetch(url, { method: 'POST', headers, body: JSON.stringify(body) });
 
       return response.ok;
     } catch {}
