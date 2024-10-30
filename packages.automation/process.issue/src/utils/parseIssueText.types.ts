b42a40d836e52033633a95948ba5fa0a3339be14
andrew robertson <andrewcrobertson@gmail.com>
2024-10-31 09:47:20 +1100

    wip

--- packages.automation/process.issue/src/utils/parseIssueText.types.ts
+++ packages.automation/process.issue/src/utils/parseIssueText.types.ts
@@ -1,16 +1,10 @@
-import { parse } from 'yaml';
+export enum ParseOutputEnum {
+  NoSeparatorFound = 'NoSeparatorFound',
+  CouldNotParseYaml = 'CouldNotParseYaml',
+  Successful = 'Successful',
+}
 
-export const parseIssueText = (text: string): Record<string, any> | null => {
-  const separatorIndex = text.indexOf('===');
-
-  if (separatorIndex === -1) return null;
-
-  const yamlContent = text.slice(separatorIndex + 3).trim();
-
-  try {
-    const output = parse(yamlContent) as Record<string, any>;
-    return output;
-  } catch (e) {
-    return null;
-  }
-};
+export type ParseIssueTextOutput =
+  | { code: ParseOutputEnum.NoSeparatorFound }
+  | { code: ParseOutputEnum.CouldNotParseYaml }
+  | { code: ParseOutputEnum.Successful; data: Record<string, any> };
